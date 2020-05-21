0e655e6126e8ad0de675024c757f85a885425c81
a19chrel <a19chrel@student.his.se>
2020-05-21 20:00:22 +0200

    Added: Static listview

--- app/src/main/java/com/a19chrel/project/MainActivity.java
+++ app/src/main/java/com/a19chrel/project/MainActivity.java
@@ -3,12 +3,26 @@ package com.a19chrel.project;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
+
+    private ArrayAdapter<String> adapter;
+    private String[] birdNames = {"Bird1", "Bird2", "Bird3"};
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        adapter = new ArrayAdapter<>(getApplicationContext(),R.layout.listview_layout,R.id.listitem,birdNames);
+        ListView mylistview = findViewById(R.id.listview);
+        mylistview.setAdapter(adapter);
+
     }
 }
