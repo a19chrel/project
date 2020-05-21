bbf3ce1fa7ba7ed6190aaaf3004555ebd531b442
a19chrel <a19chrel@student.his.se>
2020-05-21 09:38:10 +0200

    Added: Bird class

---
+++ app/src/main/java/com/a19chrel/project/Bird.java
@@ -0,0 +1,53 @@
+package com.a19chrel.project;
+
+public class Bird {
+
+    private String id;
+    private String name;
+    private int quantity;
+    private String category;
+    private String location;
+    private String pictureUrl;
+
+    public Bird(){
+        id = "unknown";
+        name = "unknown";
+        quantity = 0;
+        category = "unknown";
+        location = "unknown";
+        pictureUrl = "https://via.placeholder.com/150.jpg";
+    }
+
+    public Bird(String id, String name, int quantity, String category, String location, String pictureUrl){
+        this.id = id;
+        this.name = name;
+        this.quantity = quantity;
+        this.category = category;
+        this.pictureUrl = pictureUrl;
+    }
+
+    public String getId(){
+        return id;
+    }
+
+    public String getName(){
+        return name;
+    }
+
+    public int getQuantity(){
+        return quantity;
+    }
+
+    public String getCategory(){
+        return category;
+    }
+
+    public String getLocation(){
+        return location;
+    }
+
+    public String getPictureUrl(){
+        return pictureUrl;
+    }
+
+}
