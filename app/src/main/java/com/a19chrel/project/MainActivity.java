162cbc4cc01f1eedbdcaad3b3ff2c288181c1c35
a19chrel <a19chrel@student.his.se>
2020-05-23 13:20:28 +0200

    Added: Menu

--- app/src/main/java/com/a19chrel/project/MainActivity.java
+++ app/src/main/java/com/a19chrel/project/MainActivity.java
@@ -7,6 +7,9 @@ import android.app.ProgressDialog;
 import android.content.DialogInterface;
 import android.os.AsyncTask;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
@@ -41,14 +44,6 @@ public class MainActivity extends AppCompatActivity {
         adapter = new ArrayAdapter<>(getApplicationContext(),R.layout.listview_layout,R.id.listitem,birdNames);
         ListView mylistview = findViewById(R.id.listview);
         mylistview.setAdapter(adapter);
-
-        Button btn = findViewById(R.id.refreshbutton);
-        btn.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                new JsonTask().execute("https://wwwlab.iit.his.se/brom/kurser/mobilprog/dbservice/admin/getdataasjson.php?type=a19chrel");
-            }
-        });
 
     }
     @SuppressLint("StaticFieldLeak")
@@ -133,4 +128,32 @@ public class MainActivity extends AppCompatActivity {
             }
         }
     }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_about) {
+            Log.e( "onOptionsItemSelected: ", "About");
+            return true;
+        }
+        if (id == R.id.action_refreshlist) {
+            new JsonTask().execute("https://wwwlab.iit.his.se/brom/kurser/mobilprog/dbservice/admin/getdataasjson.php?type=a19chrel");
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
 }
